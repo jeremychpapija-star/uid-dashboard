47b8516f6084fdd72131bb64227ccccd96941207
jeremychpapija-star <jeremychpapija@gmail.com>
2025-12-11 06:46:03 -0500

    app.js

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ async function loadUIDs() {
     list.innerHTML = "";
 
     data.forEach(item => {
+        const expDate = item.expires_at 
+            ? new Date(item.expires_at).toLocaleString()
+            : "Sin expiración";
+
         list.innerHTML += `
             <tr>
                 <td>${item.uid}</td>
+                <td>${expDate}</td>
                 <td>
                     <button class="action-btn" onclick="deleteUID('${item.uid}')">Eliminar</button>
                 </td>
@@ -21,15 +26,19 @@ async function loadUIDs() {
 
 async function addUID() {
     const uid = document.getElementById("newUID").value.trim();
+    const days = parseInt(document.getElementById("days").value.trim());
+
     if (!uid) return alert("Escribe un UID");
+    if (!days) return alert("Escribe días válidos");
 
     await fetch(API_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ uid: uid, value: "1" })
+        body: JSON.stringify({ uid: uid, value: "1", days: days })
     });
 
     document.getElementById("newUID").value = "";
+    document.getElementById("days").value = "";
     loadUIDs();
 }
 
